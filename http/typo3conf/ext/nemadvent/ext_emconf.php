e1ef609282736ea5dd250a5def2a3a7746e5b831
Joerg Velletti <jvelletti@allplan.com>
2021-05-11 09:09:52 +0200

    [LTS-10] raise needed typo3 version, add scheduler to composer and some smaller changes

--- http/typo3conf/ext/nemadvent/ext_emconf.php
+++ http/typo3conf/ext/nemadvent/ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '8.7.0-8.99.99',
+			'typo3' => '8.7.0-10.99.99',
 		),
 		'conflicts' => array(
 		),

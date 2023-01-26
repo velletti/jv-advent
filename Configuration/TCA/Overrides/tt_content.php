152becdc78db33d3ea7a3e8230639ee6dad5c8ab
Joerg Velletti <jvelletti@allplan.com>
2023-01-26 12:56:11 +0100

    Fix TCA

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,9 +1,8 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-$_EXTKEY ="nemadvent" ;
-$extensionName = \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($_EXTKEY);
+$extensionName = \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase("nemadvent");
 $pluginSignature = strtolower($extensionName) . '_pi1';
 
 // plugin registrieren
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin ($_EXTKEY, 'Pi1', 'Display Advent');
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin ("nemadvent", 'Pi1', 'Display Advent');
